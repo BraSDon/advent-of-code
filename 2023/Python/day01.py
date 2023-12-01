519c55b522a9cbb061b8aeb1d6f0df9f6a67086f
Brandon <brandon@schuehlein.eu>
2023-12-01 10:40:04 +0100

    simplify day 1, puzzle 1

--- 2023/Python/day01.py
+++ 2023/Python/day01.py
@@ -1,14 +1,9 @@
 def is_digit(c) -> bool:
     return c in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-def convert_two_digit(num: str):
+def convert_two_digit(num: str) -> int:
     # convert n-digit number to two digit number
-    if len(num) == 1:
-        return int(num + num)
-    elif len(num) > 2:
-        return int(num[0] + num[-1])
-    else:
-        return int(num)
+    return int(num[0] + num[-1])
 
 with open("./inputs/day01.txt", "r") as f:
     lines = f.readlines()
