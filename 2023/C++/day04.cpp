0064710b6216db666dcde7df527b3b0e98c94648
Brandon <brandon@schuehlein.eu>
2023-12-04 21:41:27 +0100

    c++: day 4, puzzle 2

--- 2023/C++/day04.cpp
+++ 2023/C++/day04.cpp
@@ -20,6 +20,19 @@ set<int> parseLineToSet(string input) {
     return s;
 }
 
+int matchedWinning(string line, smatch match) {
+    set<int> winningSet = parseLineToSet(match[2].str());
+    set<int> mineSet = parseLineToSet(match[3].str());
+
+    vector<int> intersection;
+    set_intersection(
+        winningSet.begin(), winningSet.end(),
+        mineSet.begin(), mineSet.end(), back_inserter(intersection)
+    );
+
+    return intersection.size();
+}
+
 int main () {
     ifstream myfile("../inputs/day04.txt");
     string line;
@@ -28,30 +41,37 @@ int main () {
     // 1st group: card x
     // 2nd group: winning numbers
     // 3rd group: my numbers
-    regex card_regex("(Card.*\\d+:) *(\\d+.*) *\\| *(\\d+.*)");
+    regex card_regex("(Card.*\\d+): *(\\d+.*) *\\| *(\\d+.*)");
     smatch match;
 
-    int sum = 0;
-    int lineIndex = 0;
+    map<int, pair<int, int>> cardToFrequencyAndIntersectionSize; 
+    int sumOfPoints = 0;
+    int sumOfFrequencies = 0;
+
     while (getline(myfile, line)) {
         if (std::regex_search(line, match, card_regex)) {
-            set<int> winningSet = parseLineToSet(match[2].str());
-            set<int> mineSet = parseLineToSet(match[3].str());
-
-            vector<int> intersection;
-            set_intersection(
-                winningSet.begin(), winningSet.end(),
-                mineSet.begin(), mineSet.end(), back_inserter(intersection)
-             );
-
-            int intersectionSize = intersection.size();
-            if (intersectionSize == 0) {
-                continue;
-            }
-            sum += pow(2, intersectionSize - 1);
+            int cardNumber = stoi(match[1].str().substr(4));
+            int intersectionSize = matchedWinning(line, match);
+            cardToFrequencyAndIntersectionSize.insert({cardNumber, {1, intersectionSize}});
+            sumOfPoints += pow(2, intersectionSize - 1);
         }
     }
 
-    cout << "Sum of points: " << sum << endl;
+    for (auto& entry : cardToFrequencyAndIntersectionSize) {
+        int cardNumber = entry.first;
+        int frequency = entry.second.first;
+        int intersectionSize = entry.second.second;
+        for (int i = 0; i < intersectionSize; i++) {
+            int cardIdToBeInserted = cardNumber + i + 1;
+            cardToFrequencyAndIntersectionSize[cardIdToBeInserted].first += frequency;
+        }   
+    }
+
+    for (auto& entry : cardToFrequencyAndIntersectionSize) {
+        sumOfFrequencies += entry.second.first;
+    }
+
+    std::cout << "Sum of points: " << sumOfPoints << std::endl;
+    std::cout << "Sum of frequencies: " << sumOfFrequencies << std::endl;
     return 0;
 }
